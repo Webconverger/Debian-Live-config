4072fd8227772d35c7fb80ebce92bab99b4f831a
Chris Lamb <chris@chris-lamb.co.uk>
2008-09-08 03:55:14 +0100

    Import patch from #498143 for d-i on usb-hdd.

--- webconverger/config/binary_local-hooks/02-repack-initrd.sh
+++ webconverger/config/binary_local-hooks/02-repack-initrd.sh
@@ -13,6 +13,43 @@ gzip -d < ../${TARGET_INITRD} | cpio -i --make-directories --no-absolute-filenam
 # Overwrite banner
 mv ../binary/install/banner.png ./usr/share/graphics/logo_debian.png
 
+# Import patch from #498143
+patch -p1 <<EOF
+--- a/var/lib/dpkg/info/cdrom-detect.postinst
++++ b/var/lib/dpkg/info/cdrom-detect.postinst
+@@ -44,6 +44,28 @@ do
+ 		fi
+ 	done
+ 
++	# Try disk partitions masquerading as Debian CDs for Debian Live
++	# "usb-hdd" images. Only vfat and ext are supported.
++	modprobe vfat >/dev/null 2>&1 || true
++	for device in \$(list-devices partition); do
++		if mount -t vfat -o ro,exec \$device /cdrom ||
++		   mount -t ext2 -o ro,exec \$device /cdrom; then
++			log "Pseudo CD-ROM mount succeeded: device=\$device"
++
++			# Test whether it's a Debian CD
++			if [ -e /cdrom/.disk/info ]; then
++				mounted=1
++				db_set cdrom-detect/cdrom_device \$device
++				break
++			else
++				log "Ignoring pseudo CD-ROM device \$device - it is not a Debian CD"
++				umount /cdrom 2>/dev/null || true
++			fi
++		else
++			log "Psuedo CD-ROM mount failed: device=\$device"
++		fi
++	done
++
+ 	if [ "\$mounted" = "1" ]; then
+ 		break
+ 	fi
+-- 
+1.5.6.5
+EOF
+
 find | cpio -H newc -o | gzip -9 > ../${TARGET_INITRD}
 cd ..
 rm -rf ${REPACK_TMPDIR}
