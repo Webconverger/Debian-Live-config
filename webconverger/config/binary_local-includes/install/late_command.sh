71eafde8eba0890f7dfe782cda209135ce4fe7ad
Kai Hendry <hendry@iki.fi>
2009-08-21 01:39:51 +0200

    copy across sources.list

--- webconverger/config/binary_local-includes/install/late_command.sh
+++ webconverger/config/binary_local-includes/install/late_command.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-set -e
+#!/bin/sh -e
 
 log () {
 	logger -t webconverger_late_cmd "$@"
@@ -14,14 +12,10 @@ in-target apt-get remove --yes aufs-modules-* user-setup
 log "Removing empty xorg.conf files"
 rm -f /target/etc/X11/xorg.conf*
 
-#log "Setting Grub splash"
-#cp /cdrom/boot/grub/splash.xpm.gz /target/boot/grub/splash.xpm.gz
-#in-target update-grub
-# Generated path is wrong
-#sed -i -e 's|//grub/splash.xpm.gz|/boot/grub/splash.xpm.gz|' /target/boot/grub/menu.lst
-
-#log "Workaround for older live-initramfs versions"
-#in-target chown -R 1000:1000 /home/${USER}
+log "Setting Grub splash"
+cp /cdrom/boot/grub/splash.xpm.gz /target/boot/grub/splash.xpm.gz
+in-target update-grub
+echo "splashimage /boot/grub/splash.xpm.gz" >> /target/boot/grub/menu.lst
 
 log "Remove passwords"
 in-target passwd --delete root
@@ -29,6 +23,9 @@ in-target passwd --delete ${USER}
 
 log "Setting up autologin"
 sed -i -e "s|^\([^:]*:[^:]*:[^:]*\):.*getty.*\<\(tty[0-9]*\).*$|\1:/bin/login -f ${USER} </dev/\2 >/dev/\2 2>\&1|" /target/etc/inittab
+
+log "Setting up Webconverger sources.list"
+cp /etc/apt/sources.list /target/etc/apt/sources.list
 
 log "Flushing filesystem buffers"
 sync
