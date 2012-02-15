11425c32b5f30d3949ec8547531447df08017769
Kai Hendry <hendry@webconverger.com>
2012-02-15 09:40:55 +0100

    Rejiggle default bg.png location Add squeeze-backports for new Xorg

--- webconverger/config/includes.chroot/home/webc/pb.sh
+++ webconverger/config/includes.chroot/home/webc/pb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # pb = persistent browser
-# Keep the browser running and clean between sessions in ~webc
+# Keep the browser running and clean between sessions in $WEBCHOME
 # hendry@webconverger.com
 WEBCHOME=/home/webc
 
@@ -14,7 +14,7 @@ touch /tmp/pb-loop
 # disable bell
 xset b 0 0
 
-# white background
+# white background http://webconverger.org/artwork/
 xsetroot -solid "#ffffff"
 
 # only when noroot is supplied, we use Webc's WM dwm.web
@@ -45,13 +45,16 @@ test $1 || set -- "http://portal.webconverger.com/"
 # if no-x-background is unset, try setup a background from homepage sans query
 grep -qs noxbg /proc/cmdline || {
 wget --timeout=5 $1/bg.png -O $WEBCHOME/bg.png && file $WEBCHOME/bg.png | grep -q "image data" ||
-cp /etc/webconverger/bg.png $WEBCHOME/bg.png
+cp /etc/webc/bg.png $WEBCHOME/bg.png
 xloadimage -quiet -onroot -center $WEBCHOME/bg.png
 }
 
-# No screen blanking (warning: might limit the life span of your screen & world)
+# No screen blanking - needed for Digital signage
 grep -qs noblank /proc/cmdline && xset s off && xset -dpms && logger noblank
 
+
+# TODO: Maybe merge MAC finding code?
+# https://github.com/Webconverger/Debian-Live-config/blob/master/webconverger/config/includes.chroot/etc/network/if-up.d/ping
 for i in $(ls /sys/class/net)
 do
 	test $(basename $i) = "lo" && continue
