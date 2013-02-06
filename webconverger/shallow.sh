29a8a406392ff5dff850e506bae2d643585b32d7
Kai Hendry <hendry@webconverger.com>
2013-02-06 03:17:42 +0000

    get rid of cruft

--- webconverger/shallow.sh
+++
@@ -1,5 +0,0 @@
-test -d /tmp/chroot-old && sudo rm -rf /tmp/chroot-old
-sudo mv chroot-old /tmp/chroot-old
-sudo mv chroot chroot-old
-sudo git clone --depth 1 -b master file:///home/hendry/test chroot
-cd chroot && sudo git remote set-url origin git://github.com/Webconverger/webc.git
