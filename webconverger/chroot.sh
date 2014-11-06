d6082b4717de2c66501e63f9556bc2d9f494cd4d
Kai Hendry <hendry@webconverger.com>
2014-11-06 17:31:07 +0800

    New logo

--- webconverger/chroot.sh
+++ webconverger/chroot.sh
@@ -15,13 +15,14 @@ if test $# -gt 0
 then
 	Chroot chroot $@
 else
-sudo mount -o bind /dev chroot/dev
-sudo mount -o bind /sys chroot/sys
-sudo mount -o bind /proc chroot/proc
-sudo chroot chroot
-sudo umount chroot/sys
-sudo umount chroot/proc
-sudo umount chroot/dev
+
+	sudo mount -o bind /dev chroot/dev
+	sudo mount -o bind /sys chroot/sys
+	sudo mount -o bind /proc chroot/proc
+	sudo chroot chroot /usr/bin/env -i HOME="/root" PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" /bin/bash
+	sudo umount chroot/sys
+	sudo umount chroot/proc
+	sudo umount chroot/dev
 
 
 
